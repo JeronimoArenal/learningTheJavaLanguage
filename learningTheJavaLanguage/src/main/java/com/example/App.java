17334202de5dc2216e0afe21ec879b3e1e4ef030
JeronimoArenal <jarenal1969@gmail.com>
2026-04-08 12:44:04 +0200

    creando la clase Persona

---
+++ learningTheJavaLanguage/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        // 1. Crear el objeto (instancia)
+        Persona objeto = new Persona();
+        
+        // 2. Llamar al método que quieres ejecutar
+        //objeto.saludar();
+    }
+}
