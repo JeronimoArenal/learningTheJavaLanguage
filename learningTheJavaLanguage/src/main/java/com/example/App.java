b2ad4c6703345277dd6c6a50c4ebdcd0bb5d6a07
JeronimoArenal <jarenal1969@gmail.com>
2026-04-08 14:18:23 +0200

    Antes de estudiar los modificadores de acceso

--- learningTheJavaLanguage/src/main/java/com/example/App.java
+++ learningTheJavaLanguage/src/main/java/com/example/App.java
@@ -1,9 +1,35 @@
 package com.example;
 
 public class App {
+	
     public static void main(String[] args) {
-        // 1. Crear el objeto (instancia)
-        Persona objeto = new Persona();
+        // 1. Crear el objeto (instancia) Persona
+        Persona objetoPax1 = new Persona();
+        Persona objetoPax2 = new Persona();
+               
+        objetoPax1.nombre = "Ramiro";
+        objetoPax2.nombre = "Ramiro";
+        
+        if(objetoPax1 == objetoPax2) {
+         	System.out.println("Son iguales");
+        } else {
+        	System.out.println("No son iguales");
+        }
+        /*
+         * identityHashCode garantiza un valor único por vida del objeto.
+         * hash hexadecimal del objeto.
+         */
+        /*
+         * ID único: 366712642
+ID único: 692404036
+         */
+        
+        int referenciaInt1 = System.identityHashCode(objetoPax1);
+        System.out.println("ID único: " + referenciaInt1);
+        
+        int referenciaInt2 = System.identityHashCode(objetoPax2);
+        System.out.println("ID único: " + referenciaInt2);
+   
         
         // 2. Llamar al método que quieres ejecutar
         //objeto.saludar();
--- learningTheJavaLanguage/src/main/java/com/example/Persona.java
+++ learningTheJavaLanguage/src/main/java/com/example/Persona.java
@@ -1,5 +1,10 @@
 package com.example;
 
 public class Persona {
+	
+	String nombre;
+	String primerApellido;
+	String segundoApellido;
+	
 
 }
