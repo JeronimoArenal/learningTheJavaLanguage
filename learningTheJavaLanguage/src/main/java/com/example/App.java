1b4faf3e0111bdf7eeb497c96ec1b827c10abdef
JeronimoArenal <jarenal1969@gmail.com>
2026-04-08 14:58:40 +0200

    modificadores de acceso

--- learningTheJavaLanguage/src/main/java/com/example/App.java
+++ learningTheJavaLanguage/src/main/java/com/example/App.java
@@ -3,35 +3,14 @@ package com.example;
 public class App {
 	
     public static void main(String[] args) {
-        // 1. Crear el objeto (instancia) Persona
-        Persona objetoPax1 = new Persona();
-        Persona objetoPax2 = new Persona();
-               
-        objetoPax1.nombre = "Ramiro";
-        objetoPax2.nombre = "Ramiro";
-        
-        if(objetoPax1 == objetoPax2) {
-         	System.out.println("Son iguales");
-        } else {
-        	System.out.println("No son iguales");
-        }
-        /*
-         * identityHashCode garantiza un valor único por vida del objeto.
-         * hash hexadecimal del objeto.
-         */
-        /*
-         * ID único: 366712642
-ID único: 692404036
-         */
-        
-        int referenciaInt1 = System.identityHashCode(objetoPax1);
-        System.out.println("ID único: " + referenciaInt1);
-        
-        int referenciaInt2 = System.identityHashCode(objetoPax2);
-        System.out.println("ID único: " + referenciaInt2);
-   
-        
-        // 2. Llamar al método que quieres ejecutar
-        //objeto.saludar();
+    	
+    	Persona persona1 = new Persona();
+    	persona1.setNombre("Jerónimo");
+    	persona1.setPrimerApellido("Arenal");
+    	persona1.setSegundoApellido("Gómez");
+    	
+    	System.out.print("Me llamo " + persona1.getNombre() +' ' + persona1.getPrimerApellido() + ' ' +
+    			persona1.getSegundoApellido()); 
+
     }
 }
--- learningTheJavaLanguage/src/main/java/com/example/Persona.java
+++ learningTheJavaLanguage/src/main/java/com/example/Persona.java
@@ -1,10 +1,58 @@
 package com.example;
 
 public class Persona {
+	/*
+	 * protected: Lo ven todas las clases del mismo paquete Y 
+	 * también cualquier clase hija, sin importar en qué paquete o carpeta esté
+	 * 
+	 * default: Lo ven todas las clases del mismo paquete
+	 * 
+	 * private: se relacionan con el mundo exterior a través de métodos 
+	 * públicos que acceden a dichas variables. Una instancia de la misma clase no puede 
+	 * acceder a las propiedades privadas. Las variables de una clase deben ser privadas.
+	 */
 	
-	String nombre;
-	String primerApellido;
-	String segundoApellido;
+	private String nombre;
+	private String primerApellido;
+	private String segundoApellido;
+	
+	
+  /*  public void setNombre(String nuevoNombre) {
+        // "this.nombre" se refiere a la variable privada de arriba
+        // "nuevoNombre" es lo que recibes por paréntesis
+        this.nombre = nuevoNombre;
+    }
+	
+	
+    public String getNombre() {
+        return nombre; 
+    }
+	*/
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		// "this.nombre" se refiere a la variable privada de arriba
+		// "nombre" es lo que recibes por paréntesis
+		this.nombre = nombre;
+	}
+	public String getPrimerApellido() {
+		return primerApellido;
+	}
+	public void setPrimerApellido(String primerApellido) {
+		this.primerApellido = primerApellido;
+	}
+	public String getSegundoApellido() {
+		return segundoApellido;
+	}
+	public void setSegundoApellido(String segundoApellido) {
+		this.segundoApellido = segundoApellido;
+	}
+	
+
+	
 	
 
 }
