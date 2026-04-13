ced37355adfad4af00a302d1c3fcde1c086403ad
JeronimoArenal <jarenal1969@gmail.com>
2026-04-13 10:39:34 +0200

    Antes de ver el tipo Array

--- learningTheJavaLanguage/src/main/java/com/example/App.java
+++ learningTheJavaLanguage/src/main/java/com/example/App.java
@@ -1,40 +1,31 @@
 package com.example;
 
-import java.time.LocalDate;
-import java.time.Month;
 
 public class App {
+	//Variable de clase
+	private static int personas = 5; 
+	
+	//Variables de Instancia
+	private String nombre;		//Por defecto NULL
+	private int total;			//Por defecto 0
 	
     public static void main(String[] args) {
-    	
-    	Empleado empleado = Empleado.builder()
-	            .salario(2750.0)
-	            .dpto(Dpto.CONTABILIDAD)
-	            .fecha_alta(LocalDate.of(2000, Month.MAY, 22))
-	            .ssn("123-456-789")
-	            .build();
-    	
-    	System.out.println(empleado);
-    			
-    	
-    	 Empleado empleado3 = Empleado.builder()
-    	            // Atributos de Persona
-    	            .nombre("Jerónimo")
-    	            .primerApellido("Arenal")
-    	            .segundoApellido("Gómez")
-    	            .genero(Genero.FEMENINO)
-    	            .fechaNacimiento(LocalDate.of(1969, Month.MAY, 22))
-    	            
-    	            // Atributos de Empleado
-    	            .salario(2750.0)
-    	            .dpto(Dpto.CONTABILIDAD)
-    	            .fecha_alta(LocalDate.now())
-    	            .ssn("123-456-789")
-    	            
-    	            //Creamos el objeto
-    	            .build();
-
-    	    System.out.println(empleado3);
+    	/* VARIABLES
+    	 * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html
+    	 * Para nombrar una variable hay que tener en cuenta lo siguiente:
+    	 * 1º El primer caracter no puede ser un numero y aunque se permite $ y _ no deben usarse.
+    	 * 2º Las variables declaradas a nivel de clase, no es necesario asignaerles un valor por defecto,
+    	 * porque tiene un valor por defecto. Lo contrario ocurre con las locales en las que el programador debe
+    	 * darles un valor inicial es decir deben ser inicializadas explicitamente
+    	 */
+    	int total2 = 3;			//Primitivo
+    	Integer total3 = 4;		//Envoltorio
+    	/* TIPOS DE DATO
+    	 * 1º Primitivos .- Guardan el valor puro.
+    	 * 2º Objetos o Referencia.- guardan una referencia(direccion de memoria) donde estan los datos.
+    	 * En la mayoria de los casos debemos utilizar el tipo primitivo en lugar de su correspondiente 
+    	 * tipo Objeto o Referencia
+    	 */
 
     }
 }
