dfdbf49794ee539e79210ae64f3794e66c21c1d9
JeronimoArenal <jarenal1969@gmail.com>
2026-04-15 09:48:54 +0200

    for y formejorado con 3 metodos

--- learningTheJavaLanguage/src/main/java/com/example/App.java
+++ learningTheJavaLanguage/src/main/java/com/example/App.java
@@ -6,8 +6,12 @@ import java.util.Arrays;
 
 public class App {
 	
-	static final int TOTAL_ELEMENTOS = 100;
+	static final int TOTAL_ELEMENTOS = 10;
 	
+	static int[] numerosEnteros = new int[TOTAL_ELEMENTOS];
+	
+	
+	// .......................... Método main .......................................
     public static void main(String[] args) {
     	/* ARRAY
     	 * Es una variable de referencia que apunta a una zona de la RAM, donde se almacena un conjunto de valores 
@@ -15,15 +19,131 @@ public class App {
     	 * aunque si sus elementos. Para acceder a los elementos se utiliza un indice entre corchetes con inicio en 0.
     	 */
     	
-        mainArray();  	
-        arrayPersonas();
+    	int[] miArray = rellenarArray();	//Variable local para guaradar el retiurn del método
+    	    	
     	operatorsInt();
-    	prePost();
-    	postPre();
+    	operatorsInt(miArray);
+    	forMejorado();
+    	forMejorado(miArray);
+        //mainArray();  	
+        //arrayPersonas();
+    	//prePost();
+    	//postPre();
     	
     }
     
-	// .......................... M E T H O D S ..............................................
+    // .................... Método crearYLlenar metodo sin void por lo que devuelve array de enteros ..............................................
+    public static int[] rellenarArray() {
+        int[] numerosEnteros = new int[TOTAL_ELEMENTOS]; //Reservamos espacio en RAM para las cantidad TOTAL_ELEMENTOS
+        
+        for(int i = 0; i < TOTAL_ELEMENTOS; i++) {		//Llenamos bucle for
+            numerosEnteros[i] = i + 1;
+        }
+        return numerosEnteros; // Devolvemos al array lleno para llenar la variable
+    }
+    
+    
+	// .......................... M E T H O D S ..............................................    
+    public static void operatorsInt(){
+    	/* OPERADORES
+    	 *  Operadores Aritméticos, que son: +, -, *, /, %
+    	 *  De los anteriores operadores vamos a ejemplificar el operador %(modulus) devuelve el residuo de la división.
+    	 *  Utilizaremos la sentencia "for" para recorrer un array, primero la sentencia clásica y la mejorada.
+    	 */   	
+    	
+    	// 1º Declarsmos el array utilizando la cantidad de elementos de TOTAL_ELEMENTOS
+    	//int[] numerosEnteros = new int[TOTAL_ELEMENTOS]; //estamos utilizando la variable global (static)
+    	
+    	//2.  Agregamos valores al array
+    	for(int i = 0; i < TOTAL_ELEMENTOS; i++) {
+    		  numerosEnteros[i] = i + 1; 
+    	}
+ 
+    	//3- Recorrer el array y solamente mostrar los elementos que sean pares
+    	System.out.println(" ------------------- For clásico pares ------------------");   	
+    	for(int i = 0; i < TOTAL_ELEMENTOS; i++) {
+    	    if (numerosEnteros[i] % 2 == 0) {	// Verificamos si es impar (el resto de la división entre 2 NO es 0)
+    	        System.out.println("Posición " + i + " par: " + numerosEnteros[i]);
+    	    }
+    	}    	
+    	
+    }
+    
+    public static void operatorsInt(int[] numerosEnteros) {
+        System.out.println(" ------------------- Metodo Numeros Pares Sobrecargado (------------------");
+        for(int i = 0; i < numerosEnteros.length; i++) {
+            if (numerosEnteros[i] % 2 == 0) {
+                System.out.println("Posición: " + i + " Par: " + numerosEnteros[i]);
+            }
+        }
+    }
+    
+    //............................... Metodo For mejorado ------------------------------------
+    
+    public static void forMejorado(){
+    	int contador = 0;
+
+    	// 1º Declarsmos el aray utilizando la cantidad de elementos de TOTAL_ELEMENTOS
+    	// int[] numerosEnteros = new int[TOTAL_ELEMENTOS];
+    	
+        // 1º Comitear los cambios actuales del proyecto poniendo como comentario: "antes de realizar el ejercicio3"
+        // 2º Utilizando un bucle for mejorado, que muestre los elementos que son impares del array numerosEnteros y
+        //contando la cantidad de numeros impares:
+    	        
+        // EJERCICIO 3
+    	   	
+        // PASO 1: Llenar el array (For clásico)
+        for (int i = 0; i < TOTAL_ELEMENTOS; i++) {
+            numerosEnteros[i] = i + 1;
+        }
+    	
+    	System.out.println(" ------------------- Método For mejorado Impares ------------------");
+    	for(int numeroEntero : numerosEnteros) {
+    	    if (numeroEntero % 2 != 0) {
+    	        System.out.println("Impar: " + numeroEntero);
+    	        contador++;
+    	    }
+
+    	}
+    	System.out.println("Total números impares: " + contador);    	
+    }
+    
+        
+    public static void forMejorado(int[] numerosEnteros) {
+        System.out.println(" -------------------Mètodo forFor mejorado Impares Sobrecargado (Overloading)------------------");
+        int contador = 0;
+        for(int numeroEntero : numerosEnteros) {
+            if (numeroEntero % 2 != 0) {
+                System.out.println("Impar: " + numeroEntero);
+                contador++;
+            }
+        }
+        System.out.println("Total números impares: " + contador);
+    }
+    
+
+    
+    
+    
+    
+    
+    
+	// i++ (primero se usa el valor actual de la variable y después se le suma 1)
+	// ++i (primero se le suma 1 a la variable y después se usa el nuevo valor)
+    
+    public static void prePost() {
+    	int a = 10;
+    	int b = a++;
+    	
+    	System.out.println("a: " + a + " b: " + b );
+    }
+    
+    public static void postPre() {
+    	int a = 10;
+    	int b = ++a;
+    	
+    	System.out.println("a: " + a + " b: " + b );
+    }
     
     public static void mainArray(){
     	String[] nombres = {"Carolina", "Jorge", "Daniel"};
@@ -97,78 +217,5 @@ public class App {
             };
 
     }
-    
-    public static void operatorsInt(){
-    	/* OPERADORES
-    	 *  Operadores Aritméticos, que son: +, -, *, /, %
-    	 *  De los anteriores operadores vamos a ejemplificar el operador %(modulus) devuelve el residuo de la división.
-    	 *  Utilizaremos la sentencia "for" para recorrer un array, primero la sentencia clásica y la mejorada.
-    	 */
-    	
-    	//1- Declarar u array de 100 elementos
-    	//2- Agregar valores al array
-    	//3- Recorrer el array y solamente mostrar los elementos que sean pares
-    	
-    	int[] numerosEnteros = new int[TOTAL_ELEMENTOS];
-    	
-    	// i++ (primero se usa el valor actual de la variable y después se le suma 1)
-    	// ++i (primero se le suma 1 a la variable y después se usa el nuevo valor)
-    	
-    	for(int i = 0; i < TOTAL_ELEMENTOS; i++) {
-    		  numerosEnteros[i] = i + 1; 
-    		  System.out.println("Posición " + i + " tiene el valor: " + numerosEnteros[i]);
-    	}
-    	
-    	System.out.println(" ------------------- For mejorado ------------------");
-    	for(int numeroEntero : numerosEnteros) {
-    		System.out.println("Número: " + numeroEntero);
-    		
-    	}
-    	
-        
-        // 1º Comitear los cambios actuales del proyecto poniendo como comentario: "antes de realizar el ejercicio3"
-        // 2º Utilizando un bucle for mejorado, que muestre los elementos que son impares del array numerosEnteros y
-        //contando la cantidad de numeros impares:
-    	
-        
-        // EJERCICIO 3
-    	
-    	System.out.println(" ------------------- For mejorado Impares ------------------");
-    	
-    	int contador = 0;
-
-    	for(int numeroEntero : numerosEnteros) {
-    	    if (numeroEntero % 2 != 0) {
-    	        System.out.println("Número impar: " + numeroEntero);
-    	        contador++;
-    	    }
-    	}
-    	System.out.println("Total números impares: " + contador);
-    	
-    	System.out.println(" ------------------- Numeros Pares ------------------");
-    	
-    	for(int i = 0; i < TOTAL_ELEMENTOS; i++) {
-    	    // Verificamos si es impar (el resto de la división entre 2 NO es 0)
-    	    if (numerosEnteros[i] % 2 == 0) {
-    	        System.out.println("Posición " + i + " tiene el valor impar: " + numerosEnteros[i]);
-    	    }
-    	}
-    	
-    } 
-
-    
-    public static void prePost() {
-    	int a = 10;
-    	int b = a++;
-    	
-    	System.out.println("a: " + a + " b: " + b );
-    }
-    
-    public static void postPre() {
-    	int a = 10;
-    	int b = ++a;
-    	
-    	System.out.println("a: " + a + " b: " + b );
-    }    
     
 }
